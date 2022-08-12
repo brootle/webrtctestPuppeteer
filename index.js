6c5607a6a8be84ce443761256bb806f2a08d6bef
Alexander Cherednichenko <luganskukraine@gmail.com>
2022-08-12 16:22:03 +0300

    updated to get url for stream

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  //const browser = await puppeteer.launch(); // await puppeteer.launch({args:['--no-sandbox']});
-  const browser = await puppeteer.launch({args:['--no-sandbox']});
-  const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.screenshot({path: 'google.png'});
 
+  const url = "https://html-static.qencode.com/qencode5-portal/play-webrtc/"
+  const streamUrl = process.argv[2]
+
+  //const browser = await puppeteer.launch(); // await puppeteer.launch({args:['--no-sandbox']});
+  console.log("Launching browser...")
+  const browser = await puppeteer.launch({args:['--no-sandbox']});
+  console.log("Browser launched!")
+
+  console.log("Opening new page...")
+  const page = await browser.newPage();
+  console.log("New page opened!")
+
+  console.log("Go to ", url)
+  await page.goto(url);
+  console.log("Url opened")
+
+  console.log("Wait for 5 seconds...")
+  await page.waitForTimeout(5000)
+
+  await page.waitForSelector('#url');
+  await page.type('#url', streamUrl);
+  await page.click('#start');
+  console.log("Wait for 10 seconds...")
+  await page.waitForTimeout(10000)  
+
+  console.log("Taking screenshot...")
+  await page.screenshot({path: 'screenshot.png'});
+
+  console.log("Wait for 5 seconds...")
+  await page.waitForTimeout(5000)
+
+  console.log("Closing browser...")
   await browser.close();
 })();
