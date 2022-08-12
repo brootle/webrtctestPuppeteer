7d841cc80c174ebec04d25f86b083e8ac52569b3
Alexander Cherednichenko <luganskukraine@gmail.com>
2022-08-12 14:27:27 +0300

    updated

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  //const browser = await puppeteer.launch(); // await puppeteer.launch({args:['--no-sandbox']});
+  const browser = await puppeteer.launch({args:['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://google.com');
   await page.screenshot({path: 'google.png'});
